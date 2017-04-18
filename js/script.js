cc8e3e8b591970e13044eb18dde95b5e42c5934d
Phishback <fishback.evan@gmail.com>
2017-04-18 07:59:28 -0400

    Adding check statements to ensure elements display or not.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
+// When button is also clicked, the background will change as well or just wait 30 seconds
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 document.getElementById('loadQuote').addEventListener("click", getRandomColor, false);
 var quotes = [
@@ -8,45 +9,42 @@ var quotes = [
   {
     quote: "Poets have been mysteriously silent on the subject of cheese.",
     source: "G.K. Chesteron",
-    citation: "Alarms and Discussions",
     year: 1911,
-    tag: ", food/humor"
+    tag: "food/humor"
   },
 
   {
     quote: "The early bird gets the worm, but the second mouse gets the cheese.",
     source: "Willie Nelson",
-    citation: "Unknown",
     year: 2008,
-    tag: ", food/humor"
+    tag: "food/humor"
   },
 
   {
     quote: "Life is great. Cheese makes it better",
     source: "Avery Aames",
-    citation: " The Long Quiche Goodbye",
     year: 2010,
-    tag: ", food/humor"
+    tag: "food/humor"
 
   },
 
   {
     quote: "Age is something that doesn\'t matter, unless you are a cheese.",
     source: "Luis Bunuel",
-    citation: "Unknown",
     year: 2016,
-    tag: ", food/humor"
+    tag: "food/humor"
   },
 
   {
     quote: "I\'ve got a fever and the only prescription is more cowbell",
     source: "THE Bruce Dickinson",
-    citation: "Dynamite sound",
     year: 1976,
-    tag: ", humor"
+    tag: "humor"
   }
 
 ];
+
+
 
 // Using the quotes array object literal(s), assign each quote to a numerical value
 function getRandomQuote () {
@@ -55,14 +53,20 @@ function getRandomQuote () {
 };
 
 // Calling the getRandomNumber function to display the array's properties using the numerical value assigned by the randomNumber function.
+// *UPDATED* Now will not display element if returned as null.
 function printQuote() {
-  var randomQuote = getRandomQuote();
+  var randomQuote = getRandomQuote(quotes);
   var outDisplayQuote = '<p class="quote">' + randomQuote.quote + '</p>';
   outDisplayQuote += '<p class="source">' + randomQuote.source;
-  outDisplayQuote += '<span class="citation">' + randomQuote.citation + '</span>';
-  outDisplayQuote += '<span class="year">' + randomQuote.year + '</span>';
-  outDisplayQuote += '<span class="tag">' + randomQuote.tag + '</span>';
-  outDisplayQuote += '</p>';
+
+    if (outDisplayQuote.year !== null) {
+      outDisplayQuote += '<span class="year">' + randomQuote.year + ',' + '</span>';
+    }
+    if (outDisplayQuote.tag !== null) {
+      outDisplayQuote += '<span class="tag">' + ' ' + randomQuote.tag + '</span>';
+    }
+    outDisplayQuote += '</p>';
+
   document.getElementById('quote-box').innerHTML = outDisplayQuote;
 };
 // Setting a function to change the background color on click
@@ -77,5 +81,6 @@ function getRandomColor() {
 }
 // Call the print quote function
 // Set the intervanl "refresh" time the printQuote fucntion to 30 seconds
+// Set the brackground color to change every 30 seconds as well.
 setInterval(printQuote, 30000);
 setInterval(getRandomColor, 30000);
